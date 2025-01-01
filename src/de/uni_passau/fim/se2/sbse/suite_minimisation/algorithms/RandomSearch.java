f4d5d4d608db59abab7674940671d41e5478b791
Anis Belkacem <belkac01@ads.uni-passau.de>
2025-01-01 21:16:22 +0100

    41 commit

--- src/de/uni_passau/fim/se2/sbse/suite_minimisation/algorithms/RandomSearch.java
+++ src/de/uni_passau/fim/se2/sbse/suite_minimisation/algorithms/RandomSearch.java
@@ -40,8 +40,8 @@ public class RandomSearch<T extends Chromosome<T>> implements GeneticAlgorithm<T
         double crossoverRate = random.nextDouble();
         BiMutation mutation = new BiMutation(MutationRate); 
         BiCrossover crossover = new BiCrossover(crossoverRate); 
-        int randomSize = random.nextInt(); 
-        int maxIterations = 10;
+        int randomSize = random.nextInt(10)+1; 
+        int maxIterations = 1000;
         int currentIteration = 0;
 
         while (!stoppingCondition.searchMustStop() && currentIteration < maxIterations) {
