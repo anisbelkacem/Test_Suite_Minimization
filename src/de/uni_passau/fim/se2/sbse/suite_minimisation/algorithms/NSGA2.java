1977bbe19c30509890201e1e5d63230ed815971b
Anis Belkacem <belkac01@ads.uni-passau.de>
2025-01-03 02:25:07 +0100

    119 commit

--- src/de/uni_passau/fim/se2/sbse/suite_minimisation/algorithms/NSGA2.java
+++ src/de/uni_passau/fim/se2/sbse/suite_minimisation/algorithms/NSGA2.java
@@ -57,13 +57,13 @@ public class NSGA2<T extends Chromosome<T>> implements GeneticAlgorithm<T> {
         List<T> population = initializePopulation(1050,mutation,crossover,lenchromosome); 
         stoppingCondition.notifySearchStarted();
         List<T> offspring = generateOffspring(population,selection,mutation,crossover);
-        List<T> combinedPopulation = new ArrayList<>(population);
-        combinedPopulation.addAll(offspring);
-        
+        //List<T> combinedPopulation = new ArrayList<>(population);
+        population.addAll(offspring);
+
         while (!stoppingCondition.searchMustStop()) {
             
             stoppingCondition.notifyFitnessEvaluation();
-            List<List<T>> paretoFronts = nonDominatedSorting(combinedPopulation);
+            List<List<T>> paretoFronts = nonDominatedSorting(population);
             population=paretoFronts.get(0);
         }
 
