a41d9507c0b5c4c78fb1a582d0b00ad9a6b11636
Anis Belkacem <belkac01@ads.uni-passau.de>
2025-01-03 16:16:13 +0100

    137 commit

--- src/de/uni_passau/fim/se2/sbse/suite_minimisation/algorithms/NSGA2.java
+++ src/de/uni_passau/fim/se2/sbse/suite_minimisation/algorithms/NSGA2.java
@@ -35,8 +35,8 @@ public class NSGA2<T extends Chromosome<T>> implements GeneticAlgorithm<T> {
     public List<T> findSolution() {
         double MutationRate = random.nextDouble();
         double crossoverRate = random.nextDouble();
-        BiMutation mutation = new BiMutation(0.9); 
-        BiCrossover crossover = new BiCrossover(0.9);
+        BiMutation mutation = new BiMutation(1); 
+        BiCrossover crossover = new BiCrossover(1);
         BinaryTournamentSelection selection = new BinaryTournamentSelection(
         (c1, c2) -> {
         BiChromosome chrom1 = (BiChromosome) c1;
